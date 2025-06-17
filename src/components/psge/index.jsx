498ad87db816dbc86113010c98ac5fb9f7374a1a
Wirayuda <yudaardi01@gmail.com>
2025-06-17 10:17:45 +0700

    memperbaiki humberger menu

--- src/components/psge/index.jsx
+++ src/components/psge/index.jsx
@@ -33,17 +33,17 @@ function Index() {
         <div className="close" onClick={handleHamburgerClose}>
           <span>&#10005;</span>
         </div>
-        <ul className='nav-menu'>
+        <ul className="nav-menu">
           <li><Link to="/" >Home</Link></li>
           <li><Link to="/shop">Shop</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
         <ul className='nav-icons'>
-          <li><img src="./src/assets/account.png" alt="account" /></li>
-          <li><img src="./src/assets/search.png" alt="account" /></li>
-          <li><img src="./src/assets/like.png" alt="account" /></li>
-          <li><img src="./src/assets/shop.png" alt="account" /></li>
+          <li><img src="/account.png" alt="account" /> Account</li>
+          <li><img src="/search.png" alt="search" /> Search</li>
+          <li><img src="/like.png" alt="like" /> Like</li>
+          <li><img src="/shop.png" alt="shop" /> Shop</li>
         </ul>
       </div>
       {/* <Browse /> */}
